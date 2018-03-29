e24c25fc8c5b07f743cc67d29de738fa959e8981
TristenSr <tristenfordsr@gmail.com>
2018-03-29 12:58:28 -0400

    updating game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] {
+var letters = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
     var wins = 0;
     var loses = 0;
     var guessesLeft = 0;
     var guessesSoFar = 0;
     var userGuess = null;    
-}
-
 // computer random function input should be set
-
 var letterToBeGuessed = letters[Math.floor(Math.random() * letters.length)];
-
+console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 // document key function for computer
-
+document.onekey = function function(event) {
+var userGuess = String.fromChartCode(event.keyCode).toLowerCase();
+}
