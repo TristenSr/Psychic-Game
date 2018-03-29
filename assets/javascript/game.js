0eb1effb0e4a9da6fb9799e5dc1c81cd3aa370f6
TristenSr <tristenfordsr@gmail.com>
2018-03-29 11:58:25 -0400

    updating game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] {
-    
+    var wins = 0;
+    var loses = 0;
+    var guessesLeft = 0;
+    var guessesSoFar = 0;    
 }
