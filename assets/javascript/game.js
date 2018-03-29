9cede667e6c7a1724d41c10b96b76c2b5a73a4b9
TristenSr <tristenfordsr@gmail.com>
2018-03-29 12:06:05 -0400

    updating game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,10 @@ var letters = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "
     var wins = 0;
     var loses = 0;
     var guessesLeft = 0;
-    var guessesSoFar = 0;    
+    var guessesSoFar = 0;
+    var userGuess = null;    
 }
+
+// computer random function input should be set
+
+var letterToBeGuessed = letters[Math.floor(math.random())]
